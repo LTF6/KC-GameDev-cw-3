050a2c6576ccfb9a33d463d101b367fb725b9e90
SHB7 <ltofmhmd2004@gmail.com>
2022-10-18 20:47:25 +0300

    edited

--- Cw3/Assets/notAScript.cs
+++ Cw3/Assets/notAScript.cs
@@ -5,29 +5,41 @@ using UnityEngine;
 public class notAScript : MonoBehaviour
 {
 
-  // Status
+    // Status
     int maxPowerLvl = 100;
 
-  // Hero info
-   public string heroName = "Not a Hero";
-   public string heroSuperPower = "Nerd";
-   public int heroPowerLvl = 23;
-   public int HeroSpeed = 20;
+    // Hero info
+    public string heroName = "Not a Hero";
+    public string heroSuperPower = "Nerd";
+    public int heroPowerLvl = 23;
+    public int HeroSpeed = 20;
 
-  // Villain info
-   public string villainName = "Not a Hero";
-   public string villainSuperPower = "Extra Super Nerd";
-   public int villainPowerLvl = 37;
-   public int villainSpeed = 25;
+    // Villain info
+    public string villainName = "Not a Hero";
+    public string villainSuperPower = "Extra Super Nerd";
+    public int villainPowerLvl = 37;
+    public int villainSpeed = 25;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+    
+        // f num 1
+        double playerSpeed = 0;
+
+        void SetSpeed(double Speed)
+        {
+           playerSpeed = Speed;
+          SetSpeed(2.5);
+        }
+
+
         // if s1
         if (heroPowerLvl > villainPowerLvl)
         {
-            print("The Hero is Dead");
+            print("The villain is Dead");
         }
         else if (villainPowerLvl >= heroPowerLvl)
         {
@@ -37,37 +49,41 @@ public class notAScript : MonoBehaviour
         {
             print("they are playing rocket league i think");
         }
-
-        // f num 1
-        int playerSpeed = 0;
-
-        void SetSpeed(double speed)
-        {
-            speed = playerSpeed;
-            
-        }
-        SetSpeed(2.5);
-
+        
         // f num 2
-        void CompareSpeed()
+        void CompareSpeed() 
         {
-            if (HeroSpeed >= villainSpeed) {
-                print("fastest man life");
-            } else if (villainSpeed > HeroSpeed || HeroSpeed != villainSpeed) {
-                print("villains always on top");
-            } else if (HeroSpeed == villainSpeed){
-                print("Hero Speed =" + HeroSpeed + "," + "villain Speed =" + villainSpeed);
-            }
+           if (HeroSpeed >= villainSpeed) {
+              print("fastest man life");
+         } else if (villainSpeed > HeroSpeed || HeroSpeed != villainSpeed) {
+             print("villains always on top");
+         } else if (HeroSpeed == villainSpeed) {
+             print("Hero Speed =" + HeroSpeed + "," + "villain Speed =" + villainSpeed);
+          }
+        }   
 
 
-        }
-        CompareSpeed();
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 
+    // f num 2
+    /*void CompareSpeed() 
+    {
+        if (HeroSpeed >= villainSpeed) {
+            print("fastest man life");
+        } else if (villainSpeed > HeroSpeed || HeroSpeed != villainSpeed) {
+            print("villains always on top");
+        } else if (HeroSpeed == villainSpeed) {
+            print("Hero Speed =" + HeroSpeed + "," + "villain Speed =" + villainSpeed);
+        }
+     CompareSpeed();
+    }
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    } 
+
 }
+
